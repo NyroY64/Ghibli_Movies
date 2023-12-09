b4ae5dba62299dd4e72afb88a2c03134ca7bb176
NyroY64 <youcef.meradi09@gmail.com>
2023-12-09 20:03:30 +0100

    Finished the job need to hurry

--- app.js
+++ app.js
@@ -1,19 +1,65 @@
-function getMovieListing() {
-    fetch('https://ghibliapi.vercel.app/films').then(response => {return response.json();})
-      .then(response=>console.log(response))
-      .catch(error => {
-        console.error('Error:', error);
-      });
+const apiUrl ='https://ghibliapi.vercel.app/films';
+
+
+
+
+  async function getMovieListing(apiUrl) {
+    try {
+      // Fetch data from the API
+      const response = await fetch(apiUrl);
+  
+      // Check if the request was successful (status code 200)
+      if (!response.ok) {
+        throw new Error(`Failed to fetch data: ${response.status}`);
+      }
+  
+      // Parse the JSON response
+      const data = await response.json();
+  
+      // Convert the object into an array (assuming the object has enumerable properties)
+      const dataArray = Object.entries(data).map(([key, value]) => ({ key, value }));
+  
+      return dataArray;
+    } catch (error) {
+      console.error('Error fetching data:', error.message);
+      // You might want to handle errors differently based on your use case
+      return [];
+    }
   }
 
-  getMovieListing();
 
-function showMovies() {
-  const movieList=getMovieListing();
-
-  movieList.forEach(movie => {
-    const 
+  async function displayMovies() {
+    const movieListContainer = document.querySelector('.movieList');
+    const Movies = await  getMovieListing(apiUrl);
     
-  });
+    
+    console.log(Movies);
+    let parsedMovies=Movies.map((movie)=>movie=movie.value);
+
+    parsedMovies.forEach(movie => {
+
+      const movieCard = document.createElement('div');
+      movieCard.className = 'movieCard';
   
-}
+      const movieImage = document.createElement('img');
+      movieImage.className = 'movieImage';
+      movieImage.src = movie.image;
+      movieImage.alt = movie.title;
+  
+      const releaseDate=document.createElement('div');
+      releaseDate.textContent=movie.release_date;
+  
+  
+      const movieTitle = document.createElement('div');
+      movieTitle.className = 'movieTitle';
+      movieTitle.textContent = movie.title;
+  
+      
+      movieCard.appendChild(movieImage);
+      movieCard.appendChild(movieTitle);
+      movieCard.appendChild(releaseDate);
+      movieListContainer.appendChild(movieCard);
+    });
+  }
+  
+  displayMovies();
